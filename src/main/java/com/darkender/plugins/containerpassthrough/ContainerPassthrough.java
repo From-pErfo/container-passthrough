d60cc8ad25b0ea865bec4288e4f0b3ae9b95ca43
ApliNi <Nm70727@qq.com>
2023-04-21 16:18:50 +0800

    代码格式

--- src/main/java/com/darkender/plugins/containerpassthrough/ContainerPassthrough.java
+++ src/main/java/com/darkender/plugins/containerpassthrough/ContainerPassthrough.java
@@ -18,6 +18,7 @@ import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.DoubleChestInventory;
 import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.RayTraceResult;
@@ -25,8 +26,7 @@ import org.bukkit.util.RayTraceResult;
 import java.util.HashSet;
 import java.util.Set;
 
-public class ContainerPassthrough extends JavaPlugin implements Listener
-{
+public class ContainerPassthrough extends JavaPlugin implements Listener {
     private Set<EntityType> passthroughEntities;
     private Set<Material> passthroughBlocks;
     private Set<Material> dyeItems;
@@ -34,42 +34,44 @@ public class ContainerPassthrough extends JavaPlugin implements Listener
     private ReflectionUtils reflectionUtils;
     
     @Override
-    public void onEnable()
-    {
+    public void onEnable() {
+        // 初始化需要穿透和需要绕过的物品
+
+        // 实体方块: 画, 展示框, 发光展示框
+        // 普通方块: 告示牌
         passthroughEntities = new HashSet<>();
+        passthroughBlocks = new HashSet<>();
         passthroughEntities.add(EntityType.PAINTING);
         passthroughEntities.add(EntityType.ITEM_FRAME);
         passthroughEntities.add(EntityType.GLOW_ITEM_FRAME);
-        
-        passthroughBlocks = new HashSet<>();
-        for(Material material : Material.values())
-        {
-            if(material.name().contains("SIGN"))
-            {
-                passthroughBlocks.add(material);
-            }
-        }
+
+        // 手持这些物品时禁用穿透
         dyeItems = new HashSet<>();
-        for(Material material : Material.values())
-        {
-            if(material.name().contains("DYE"))
-            {
-                dyeItems.add(material);
-            }
-        }
         dyeItems.add(Material.INK_SAC);
         dyeItems.add(Material.COCOA_BEANS);
         dyeItems.add(Material.LAPIS_LAZULI);
         dyeItems.add(Material.BONE_MEAL);
         dyeItems.add(Material.GLOW_INK_SAC);
 
+        // 遍历物品枚举类, 补充其他物品
+        for(Material material : Material.values()) {
+            // 染料
+            if(material.name().contains("DYE")) {
+                dyeItems.add(material);
+            }
+            // 告示牌
+            else if(material.name().contains("SIGN")) {
+                passthroughBlocks.add(material);
+            }
+        }
+
         getServer().getPluginManager().registerEvents(this, this);
         reflectionUtils = new ReflectionUtils();
     }
     
-    private boolean canOpenContainer(Player player, Block block, BlockFace face)
-    {
+    private boolean canOpenContainer(Player player, Block block, BlockFace face) {
         // Send out an event to ensure container locking plugins aren't bypassed
+        // 发送事件以确保容器锁定插件未被绕过
         ignoreInteractEvents = true;
         PlayerInteractEvent interactEvent = new PlayerInteractEvent(player, Action.RIGHT_CLICK_BLOCK,
                 player.getInventory().getItemInMainHand(), block, face, EquipmentSlot.HAND);
@@ -78,11 +80,9 @@ public class ContainerPassthrough extends JavaPlugin implements Listener
         return !(interactEvent.useInteractedBlock() == Event.Result.DENY || interactEvent.isCancelled());
     }
 
-    private boolean tryOpeningContainerRaytrace(Player player)
-    {
+    private boolean tryOpeningContainerRaytrace(Player player) {
         RayTraceResult result = player.rayTraceBlocks(5.0, FluidCollisionMode.NEVER);
-        if(result == null || result.getHitBlock() == null || (!(result.getHitBlock().getState() instanceof Container) && !(result.getHitBlock().getState().getType().toString() == Material.CRAFTING_TABLE.toString()) && !(result.getHitBlock().getState().getType().toString() == Material.ENDER_CHEST.toString())))
-        {
+        if(result == null || result.getHitBlock() == null || (!(result.getHitBlock().getState() instanceof Container) && !(result.getHitBlock().getState().getType().toString() == Material.CRAFTING_TABLE.toString()) && !(result.getHitBlock().getState().getType().toString() == Material.ENDER_CHEST.toString()))) {
             return false;
         }
         if(result.getHitBlock().getState().getType().toString() == Material.CRAFTING_TABLE.toString()) {
@@ -95,69 +95,59 @@ public class ContainerPassthrough extends JavaPlugin implements Listener
         }
         Container container = (Container) result.getHitBlock().getState();
         
-        if(canOpenContainer(player, result.getHitBlock(), result.getHitBlockFace()))
-        {
+        if(canOpenContainer(player, result.getHitBlock(), result.getHitBlockFace())) {
             tryOpeningContainer(player, container);
         }
         return true;
     }
-    
-    private void tryOpeningContainer(Player player, Container container)
-    {
-        if(!player.getOpenInventory().getTopInventory().equals(container.getInventory()))
-        {
+
+    // 尝试打开容器
+    private void tryOpeningContainer(Player player, Container container) {
+        if(!player.getOpenInventory().getTopInventory().equals(container.getInventory())) {
             player.openInventory(container.getInventory());
-            if(container.getInventory() instanceof DoubleChestInventory)
-            {
+            if(container.getInventory() instanceof DoubleChestInventory) {
                 player.setMetadata("doublechest-open", new FixedMetadataValue(this, true));
             }
         }
     }
     
-    private void closeDoubleChest(DoubleChest doubleChest, HumanEntity entity)
-    {
+    private void closeDoubleChest(DoubleChest doubleChest, HumanEntity entity) {
         Chest left = (Chest) doubleChest.getLeftSide();
         Chest right = (Chest) doubleChest.getRightSide();
     
         // Fix old spigot bugs...
-        Bukkit.getScheduler().runTaskLater(this, () ->
-        {
-            if(reflectionUtils.isReflectionReady() && reflectionUtils.getViewers(left) != left.getInventory().getViewers().size())
-            {
+        Bukkit.getScheduler().runTaskLater(this, () -> {
+            if(reflectionUtils.isReflectionReady() && reflectionUtils.getViewers(left) != left.getInventory().getViewers().size()) {
                 reflectionUtils.closeContainer(left, entity);
                 reflectionUtils.closeContainer(right, entity);
             }
         }, 4L);
         
     }
-    
+
+    // 玩家交互实体
     @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
-    private void onPlayerInteractEntity(PlayerInteractEntityEvent event)
-    {
+    private void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
         if(event.getPlayer().isSneaking() ||
                 !passthroughEntities.contains(event.getRightClicked().getType()) ||
-                event.getHand() == EquipmentSlot.OFF_HAND)
-        {
+                event.getHand() == EquipmentSlot.OFF_HAND) {
             return;
         }
         
-        if(tryOpeningContainerRaytrace(event.getPlayer()))
-        {
+        if(tryOpeningContainerRaytrace(event.getPlayer())) {
             event.setCancelled(true);
         }
     }
-    
+
+    // 玩家交互方块
     @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
-    private void onPlayerInteract(PlayerInteractEvent event)
-    {
+    private void onPlayerInteract(PlayerInteractEvent event) {
         if(ignoreInteractEvents || event.getPlayer().isSneaking() ||
                 event.getAction() != Action.RIGHT_CLICK_BLOCK ||
                 event.getClickedBlock() == null ||
                 event.getHand() == EquipmentSlot.OFF_HAND ||
                 !passthroughBlocks.contains(event.getClickedBlock().getType()) ||
-                dyeItems.contains(event.getPlayer().getInventory().getItemInMainHand().getType()))
-
-        {
+                dyeItems.contains(event.getPlayer().getInventory().getItemInMainHand().getType())) {
             return;
         }
         Block blockBehind = event.getClickedBlock().getRelative(event.getBlockFace().getOppositeFace());
@@ -179,13 +169,12 @@ public class ContainerPassthrough extends JavaPlugin implements Listener
         tryOpeningContainer(event.getPlayer(), container);
         event.setCancelled(true);
     }
-    
+
+    // 库存关闭
     @EventHandler(ignoreCancelled = true)
-    private void onInventoryClose(InventoryCloseEvent event)
-    {
+    private void onInventoryClose(InventoryCloseEvent event) {
         HumanEntity p = event.getPlayer();
-        if(!p.hasMetadata("doublechest-open"))
-        {
+        if(!p.hasMetadata("doublechest-open")) {
             return;
         }
         DoubleChest doubleChest = (DoubleChest) event.getInventory().getHolder();
